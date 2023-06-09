43a02d4c0acfcb236a4568369b0697469d167eb7
Slakbal <leslie.price78@gmail.com>
2023-06-09 13:17:13 +0200

    Update datetime-picker.blade.php

--- resources/views/date/datetime-picker.blade.php
+++ resources/views/date/datetime-picker.blade.php
@@ -6,7 +6,7 @@
             x-ref="DateTimeInput"             
             type="text" 
             data-input
-            {!! $attributes->merge(['class' => 'form-input block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}
+            {!! $attributes->merge(['class' => 'form-input block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-200 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}
             />
 </div>
 
