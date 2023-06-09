35bd3e790120191a96ab5f2ed58a2a88c90695ee
Slakbal <leslie.price78@gmail.com>
2023-06-09 13:16:40 +0200

    Update field.blade.php

--- resources/views/components/text/field.blade.php
+++ resources/views/components/text/field.blade.php
@@ -1,1 +1,1 @@
-<textarea {!! $attributes->merge(['class' => 'form-input block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!} ></textarea>
+<textarea {!! $attributes->merge(['class' => 'form-input block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-200 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!} ></textarea>
