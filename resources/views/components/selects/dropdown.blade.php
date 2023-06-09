3f70b6c3ec1c5c503d382e1d50a3f8a69253dcbc
Slakbal <leslie.price78@gmail.com>
2023-06-09 13:16:18 +0200

    Update dropdown.blade.php

--- resources/views/components/selects/dropdown.blade.php
+++ resources/views/components/selects/dropdown.blade.php
@@ -1,6 +1,6 @@
 @props(['options' => null])
 
-<select {!! $attributes->merge(['class' => 'form-input block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}>
+<select {!! $attributes->merge(['class' => 'form-input block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-200 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}>
     @if ($options !== null)
         @forelse ($options as $key => $option)
             <option value="{{ $key }}">
