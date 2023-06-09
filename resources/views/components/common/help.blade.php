4be366b9652b7911b8bd192b07fef76318b1f627
Slakbal <leslie.price78@gmail.com>
2023-06-09 13:15:15 +0200

    Update help.blade.php

--- resources/views/components/common/help.blade.php
+++ resources/views/components/common/help.blade.php
@@ -1,3 +1,3 @@
 @props(['value'])
 
-<p {{ $attributes->merge(['class' => 'text-sm text-slate-400 dark:text-slate-200 mt-1']) }}>{{ $value ?? $slot }}</p>
+<p {{ $attributes->merge(['class' => 'block text-sm text-gray-700 dark:text-gray-200 mt-1']) }}>{{ $value ?? $slot }}</p>
