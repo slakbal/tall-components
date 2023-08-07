eaf21d4ff4cce5bb47493be1d719c97a0bc72094
Leslie Price <leslie.price78@gmail.com>
2023-08-07 11:37:09 +0200

    force numeric values to string

--- resources/views/components/selects/dropdown.blade.php
+++ resources/views/components/selects/dropdown.blade.php
@@ -3,7 +3,7 @@
 <select {!! $attributes->merge(['class' => 'form-input block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-200 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}>
     @if ($options !== null)
         @forelse ($options as $key => $option)
-            <option value="{{ $key }}">
+            <option value="{{ strval($key) }}">
                 {{ $option }}
             </option>
         @empty
