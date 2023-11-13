bf5726dda06ac231a2638c75e22a2dd470bb6dd2
Slakbal <leslie.price78@gmail.com>
2023-11-13 18:58:41 +0200

    Update select.blade.php

--- resources/views/components/selects/select.blade.php
+++ resources/views/components/selects/select.blade.php
@@ -2,7 +2,7 @@
 
 <div>
     @if($label)<x-input-label for="{{$name}}" :value="$label" />@endif
-    <x-tall-components::selects.dropdown {{$attributes}} />
+    <x-tall-components::selects.dropdown name="{{$name}}" id="{{$name}}" {{$attributes}} />
     @if($help)<x-tall-components::common.help for="{{$name}}" :value="$help" />@endif
     <x-input-error class="mt-2" :messages="$errors->get($name)" />
 </div>
